5fc85acd7592499d2ca58c67dafa438384c6c242
Anton <sortech-sorgamer@yandex.ru>
2018-11-06 08:09:33 +0300

    add sum main check

--- lab_03_1/matrix.c
+++ lab_03_1/matrix.c
@@ -4,30 +4,50 @@
 
 matrix sum(matrix a, matrix b)
 {
-	matrix c = { a.nr, a.nc, 0, NULL, NULL, NULL};
+	matrix c = { a.nr, a.nc, 0, NULL, NULL, NULL };
 	
 	c.a = calloc(a.nk + b.nk, sizeof(int));
 	c.ja = calloc(a.nk + b.nk, sizeof(int));
 	c.ia = calloc(a.nr, sizeof(int));
 	
-	int acur = 0;
-	int bcur = 0;
-	for (int i = 0; i < c.nr; i++)
+	int acur = 0, bcur = 0;
+	int ar = 0, br = 0;
+	while (a.ia[ar] != 0)
+		ar++;
+	while (b.ia[br] != 0)
+		br++;
+	
+	c.ia[0] = -1;
+	while (acur < a.nk && bcur < b.nk)
 	{
-		int ilast = ma.nk;
-		int cj = 0;
-		if (i < ma.nr - 1)
-			ilast = ma.ia[i + 1];
-		for (; ck < ilast; ck++)
+		if (ar == br && a.ja[acur] == b.ja[bcur])
 		{
-			for (; cj < ma.ja[ck]; cj++)
-				fprintf(f, "0 ");
-			fprintf(f, "%d ", ma.a[ck]);
-			cj++;
+			c.a[c.nk] = a.a[acur] + b.a[bcur];
+			c.ja[c.nk] = a.ja[acur];
+			if (c.ia[ar] == -1)
+				c.ia[ar] = c.nk
+			c.nk++;
+			acur++;
+			bcur++;
 		}
-		for (; cj < ma.nc; cj++)
-			fprintf(f, "0 ");
-		fprintf(f, "\n");
+		else if (ar <= br && a.ja[acur] <= b.ja[bcur])
+		{
+			c.a[c.nk] = a.a[acur];
+			c.ja[c.nk] = a.ja[acur];
+			if (c.ia[ar] == -1)
+				c.ia[ar] = c.nk
+			c.nk++;
+			acur++;
+		}
+		else if (br <= ar && b.ja[bqur] <= a.ja[acur])
+		{
+			c.a[c.nk] = b.a[bcur];
+			c.ja[c.nk] = b.ja[bcur];
+			if (c.ia[br] == -1)
+				c.ia[br] = c.nk
+			c.nk++;
+			bcur++;
+		}
 	}
 	
 	return c;
