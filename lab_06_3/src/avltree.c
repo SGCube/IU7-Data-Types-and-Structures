d1a71cbbcf8593c5532ecf525f71ca9236672868
Anton <sortech-sorgamer@yandex.ru>
2018-12-10 20:00:53 +0300

    height & balance fix

--- lab_06_3/src/avltree.c
+++ lab_06_3/src/avltree.c
@@ -23,31 +23,37 @@ tree_t *rotate_right(tree_t *node)
 
 ARR_DLL int ARR_DECL height(tree_t *root)
 {
+	if (!root)
+		return 0;
 	int hleft = 0, hright = 0;
-	if (root->left)
-		hleft += height(root->left);
-	if (root->right)
-		hright += height(root->right);
+	if (root && root->left)
+		hleft = height(root->left);
+	if (root && root->right)
+		hright = height(root->right);
 	return (hleft < hright) ? hright + 1 : hleft + 1;
 }
 
 ARR_DLL tree_t* ARR_DECL balance(tree_t *tree)
 {
+	if (!tree)
+		return NULL;
 	if (tree->left)
 		tree->left = balance(tree->left);
 	if (tree->right)
 		tree->right = balance(tree->right);
-	int bfactor = height(tree->left) - height(tree->right);
+	int bfactor = height(tree->right) - height(tree->left);
+	printf("%s -> bfactor: %d\t", tree->data, bfactor);
+	printf("left: %d\tright: %d\n", height(tree->left), height(tree->right));
 	if (bfactor < -1)
 	{
-		bfactor = height(tree->left->left) - height(tree->left->right);
+		bfactor = height(tree->left->right) - height(tree->left->left);
 		if (bfactor > 1)
 			tree->left = rotate_left(tree->left);
 		return rotate_right(tree);
 	}
 	else if (bfactor > 1)
 	{
-		bfactor = height(tree->right->left) - height(tree->right->right);
+		bfactor = height(tree->right->right) - height(tree->right->left);
 		if (bfactor < -1)
 			tree->right = rotate_right(tree->right);
 		return rotate_left(tree);
