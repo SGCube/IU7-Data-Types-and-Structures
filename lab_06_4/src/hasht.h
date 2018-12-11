30ed7262849d1b04b086f8b6f0ffaf5e9b6463ab
Anton <sortech-sorgamer@yandex.ru>
2018-12-11 08:31:59 +0300

    makefile change

---
+++ lab_06_4/src/hasht.h
@@ -0,0 +1,15 @@
+#ifndef __HASHT__H__
+
+#define __HASHT__H__
+
+#ifdef ARR_EXPORTS
+#define ARR_DLL __declspec(dllexport)
+#else
+#define ARR_DLL __declspec(dllimport)
+#endif
+
+#define ARR_DECL __cdecl
+
+
+
+#endif
